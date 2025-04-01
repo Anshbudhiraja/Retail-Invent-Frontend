8a3e68e39a112606c33f47c6a33be0a4bbe5182c
AnshBudhiraja7 <Anshbudhiraja77@gmail.com>
2025-04-01 22:39:13 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,15 @@
-// import path from "path"
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base:"/",
-//   build:{
-//     outDir:"dist"
-//   },
-//   plugins: [
-//     react(),
-//     tailwindcss()
-//   ],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()], 
+  base:"/",
+  build:{
+    outDir:"dist"
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+    },
+  },
+})
