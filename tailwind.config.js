0cabd71b4b9ea46c0c6faac8de2ad01f4772687b
AnshBudhiraja7 <Anshbudhiraja77@gmail.com>
2025-04-01 22:34:17 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 const colors = require("tailwindcss/colors");
 
 export default {
+  content: ["./index.html", "./src//*.{js,ts,jsx,tsx}"], // 👈 Add this to scan files
   theme: {
     extend: {
       colors: {
-        primarycolor: "#795ded", // Custom primary color
-        primaryblack: "#251F47", // Custom text color
-        primarygraylight: "#999999", // Custom gray text color
+        primarycolor: "#795ded",
+        primaryblack: "#251F47",
+        primarygraylight: "#999999",
       },
       fontFamily: {
-        sans: ["Urbanist", "sans-serif"], // Set Urbanist as the default sans font
+        sans: ["Urbanist", "sans-serif"],
       },
     },
   },
+  plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+// import path from "path"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base:"/",
+//   build:{
+//     outDir:"dist"
+//   },
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base:"/",
-  build:{
-    outDir:"dist"
-  },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+    },
+  },
+});
